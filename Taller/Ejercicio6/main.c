d530098691d1e74ed1540ee001c54967d157c95a
Miguel idarraga <Miguel.idarraga01@uceva.edu.co>
2025-08-19 16:29:04 -0500

    arregle el ejercicio 6

--- Taller/Ejercicio6/main.c
+++ Taller/Ejercicio6/main.c
@@ -1,56 +1,49 @@
-#define F_CPU 20000000
+#define F_CPU 20000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
 #define BTN_PEATONAL PD0
 
-#define VERDE_VEHICULO (1 << PA0)
+#define VERDE_VEHICULO    (1 << PA0)
 #define AMARILLO_VEHICULO (1 << PA1)
-#define ROJO_VEHICULO (1 << PA2)
-#define VERDE_PEATON (1 << PA3)
+#define ROJO_VEHICULO     (1 << PA2)
+#define VERDE_PEATON      (1 << PA3)
 
 int main(void) {
-	
+	// Botón como entrada con pull-up interno
 	DDRD &= ~(1 << BTN_PEATONAL);
 	PORTD |= (1 << BTN_PEATONAL);
 
+	// LEDs como salida
 	DDRA |= VERDE_VEHICULO | AMARILLO_VEHICULO | ROJO_VEHICULO | VERDE_PEATON;
 
 	uint8_t peatones_piden_cruce = 0;
 
 	while (1) {
-		
+		// TEST: mostrar estado real del botón en ROJO_VEHICULO
 		if (!(PIND & (1 << BTN_PEATONAL))) {
+			PORTA |= ROJO_VEHICULO;   // botón presionado
 			peatones_piden_cruce = 1;
+			} else {
+			PORTA &= ~ROJO_VEHICULO;  // botón suelto
 		}
 
-		
+		// Secuencia normal del semáforo
 		PORTA = VERDE_VEHICULO;
-		PORTA &= ~VERDE_PEATON;
 		_delay_ms(5000);
 
-		
 		PORTA = AMARILLO_VEHICULO;
-		PORTA &= ~VERDE_PEATON;
 		_delay_ms(2000);
 
-		
 		PORTA = ROJO_VEHICULO;
 
 		if (peatones_piden_cruce) {
-			
 			PORTA |= VERDE_PEATON;
-
 			_delay_ms(5000);
-
-			
 			PORTA &= ~VERDE_PEATON;
-
 			peatones_piden_cruce = 0;
 			} else {
-			PORTA &= ~VERDE_PEATON;
 			_delay_ms(5000);
 		}
 	}
 }
-
